15b47dbe1d8e52004e5313295dd4d0277cdc9f33
cadeeastlack <47193589+cadeeastlack@users.noreply.github.com>
2019-02-01 13:11:45 -0800

    Unit00

---
+++ Unit00/src/AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name Cade Eastlack \t  date 01/31/19 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
