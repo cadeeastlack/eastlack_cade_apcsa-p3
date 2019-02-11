099dd876bc90f6a5ba7c5a5f29f24d3b2057ccbc
cadeeastlack <47193589+cadeeastlack@users.noreply.github.com>
2019-02-11 12:38:51 -0800

    Unit 4 completed

---
+++ Unit04/src/DiscountRunner.java
@@ -0,0 +1,36 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - 
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class DiscountRunner
+{
+	public static void main( String args[] )
+	{
+		Scanner keyboard = new Scanner(System.in);
+				
+		out.print("Enter the original bill amount :: ");
+		double amt = keyboard.nextDouble();
+			
+		if(amt > 2000) {
+			out.print("Bill after discount :: ");
+			out.print(String.format("%.2f", Discount.getDiscountedBill(amt)));
+		}
+		
+		else {
+			out.print("Bill after discount :: ");
+			out.print(String.format("%.2f", Discount.noDiscountedBill(amt)));
+			}
+			
+		
+	}
+	
+	
+	
+}
